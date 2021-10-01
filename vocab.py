672650154abfa07394329f88e96fedf3c8ce68fb
zciw <zciw@me.com>
2021-10-01 07:07:00 +0200

    some work done

--- vocab.py
+++ vocab.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vocab.db'
 db = SQLAlchemy(app)
 
+success = False
+
 # /// three slashes means relative path
 
 class Vocab(db.Model):
@@ -27,31 +29,33 @@ for i in all:
     q_and_a.append(local_q)
 
 @app.route("/", methods=['GET', 'POST'])
-def play():
-    global q_a
-    if request.method == 'POST':
+def play():    
+    if request.method == 'POST' and 'userQuestion' not in request.form:
+        q = request.form['question']
+        a = request.form['answer']
+        item = Vocab(question=q, answer=a)
+        db.session.add(item)
+        db.session.commit()
+        for i in all:
+            local_q = i.question
+            local_a = i.answer
+            q_a[local_q] = local_a
+        return render_template("spa.html")
+    elif request.method == 'POST':
+        #global q_a
+        global success
         success = False
-        if 'userQuestion' in request.form:
-            target = request.form['userQuestion']
-            print(f'target is {target}')
-            if target == q_a[q_and_a[0]]:
-                print('success')
-                success = True
-            else:
-                print('fucked')
-                success = False
-          #  return str(success)
+        target = request.form['userQuestion']
+        print(f'target is {target}')
+        if target == q_a[q_and_a[0]]:
+            print('success')
+            success = True
         else:
-            q = request.form['question']
-            a = request.form['answer']
-            item = Vocab(question=q, answer=a)
-            db.session.add(item)
-            db.session.commit()
-            for i in all:
-                local_q = i.question
-                local_a = i.answer
-                q_a[local_q] = local_a
-    return render_template("spa.html")
+            print('fucked')
+            success = False
+        return render_template('spa.html', success= str(success))     
+    elif request.method == 'GET':
+        return render_template("spa.html")
 
 @app.route("/<section>", methods=['GET', 'POST'])
 def rsplit(section):
@@ -66,37 +70,6 @@ def rsplit(section):
     elif section == 'page2':
         return q_to_show[0]
     elif section == 'page4':
-        print('page4')
-        return 'page4'
+        return str(success)
     else:
         return jsonify(q_to_show)
-
-
-# @app.route("/play_two")
-# def play_two():
-#    return render_template('play_two.html', n=request.args.get('n', 'bmf'))
-
-# @app.route("/", methods=['GET', 'POST'])
-# def run():
-#     if request.method == 'POST':
-#         q = request.form['question']
-#         a = request.form['answer']
-#         item = Vocab(question=q, answer=a)
-#         db.session.add(item)
-#         db.session.commit()
-#         all = Vocab.query.all()
-#         global q_a
-#         for i in all:
-#             local_q = i.question
-#             local_a = i.answer
-#             q_a[local_q] = local_a
-#     return render_template("main.html")    
-
-# @app.route("/<section>", methods=['GET', 'POST'])
-# def split(section):
-#     if section == 2:
-#         user_question=list(q_a.keys())[0]
-#         return make_response(jsonify({'user_question':user_question}))
-#     elif section == 3:
-#         return make_response(jsonify(q_a))
-        
