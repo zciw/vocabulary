2ce39db6a5037c6eaa09b7334667a25ef03be3eb
Jacek Bielewicz <jacekbielewicz@Jaceks-Mac-mini.local>
2022-01-09 20:48:07 +0100

    new code from macbook

--- static/style.js
+++ static/style.js
@@ -1,7 +1,7 @@
 function toggle(element) {
     console.log('toggle')
     element.classList.toggle('open');
-    let content = document.querySelector('#nav').innerHTML;
+    let content = document.querySelector('#navl').innerHTML;
     let mobileContent = document.querySelector('#mobile-nav');
     if (mobileContent.innerHTML === content) {
         mobileContent.innerHTML = '';
